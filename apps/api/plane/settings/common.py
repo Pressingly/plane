a7e62a1ce4e694ef0d9b9faef8f6f222bbf7f352
Usama Sadiq <usama7274@gmail.com>
2026-07-07 23:23:00 +0500

    Add corporate_id check to ProxyAuth middleware

--- apps/api/plane/settings/common.py
+++ apps/api/plane/settings/common.py
@@ -106,6 +106,7 @@ DEFAULT_EMAIL_DOMAIN = os.environ.get("DEFAULT_EMAIL_DOMAIN", "askii.ai")
 # SMB portal hostname segment (landing / logout redirects) vs default Plane workspace slug
 SMB_NAME = os.environ.get("SMB_NAME")
 SMB_DEFAULT_WORKSPACE_NAME = os.environ.get("SMB_DEFAULT_WORKSPACE_NAME") or SMB_NAME
+SMB_CORPORATE_ID = os.environ.get("SMB_CORPORATE_ID")
 
 # Middlewares
 MIDDLEWARE = [
